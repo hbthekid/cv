dde51f9659850294bf8ceb1e485943151e57742a
Enrique Carvajal <enriquecarvajal@MacBook-Pro-de-Enrique.local>
2020-11-11 17:59:55 -0300

    cv

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+
+let menuIsOpen = false;
+
+function openMenu(){
+    const sideBar = document.querySelector('#sideBar');
+
+    if( menuIsOpen ){
+        sideBar.classList.remove('side-bar-open');
+        sideBar.classList.add('side-bar-close');
+        menuIsOpen = false;
+    }else{
+        sideBar.classList.remove('side-bar-close');
+        sideBar.classList.add('side-bar-open');
+        menuIsOpen = true;
+    }
+}
+
+function handleScroll(id){
+    /*    
+    const section = document.querySelector(`#${id}`).offsetTop;
+    console.log(section);
+    scroll({ top:section, behavior: "smooth"})
+    */
+   const section = document.querySelector(`#${id}`).scrollIntoView({behavior:"smooth"});
+
+}
